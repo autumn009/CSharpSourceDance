9290dcecaa1607d70bdb152000982036fc9a6428
Akira Kawamata <autumn@piedey.co.jp>
2018-12-03 19:53:45 +0900

    add abstract

---
+++ Answers/LanguageKeywords/abstract/abstract/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class ClassA
+{
+    public abstract void Sub();
+}
+
+class ClassB : ClassA
+{
+    public override void Sub()
+    {
+        Console.WriteLine("I'm ClassB.Sub!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ClassA a = new ClassB();
+        a.Sub();
+    }
+}
