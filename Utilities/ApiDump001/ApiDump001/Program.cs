3ad3561582848fe61ce781a463a088ddcff65b30
Akira Kawamata <autumn@piedey.co.jp>
2018-12-03 16:55:30 +0900

    Add ApiDump Project

---
+++ Utilities/ApiDump001/ApiDump001/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ApiDump001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
