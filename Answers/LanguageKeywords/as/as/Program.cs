85129e15df9b06ffaa3f935ed268ff90ec8b09c4
Akira Kawamata <autumn@piedey.co.jp>
2018-12-15 11:46:06 +0900

    add as

---
+++ Answers/LanguageKeywords/as/as/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        object[] array = { 123, "123", new StringBuilder("123") };
+        foreach (var item in array)
+        {
+            var val = item as string;
+            if (val == null)
+                Console.WriteLine($"{item.GetType().Name}型は'as string'でstring型に変換できません。");
+            else
+                Console.WriteLine($"{item}は長さ{val.Length}のstring型文字列です。");
+        }
+    }
+}
