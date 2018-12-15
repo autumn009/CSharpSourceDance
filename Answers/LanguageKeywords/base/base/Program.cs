ae0fe84ed83a2b117594e510c9d0bd8806140166
Akira Kawamata <autumn@piedey.co.jp>
2018-12-15 15:46:22 +0900

    add base

---
+++ Answers/LanguageKeywords/base/base/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Alpha
+{
+    public string Name { get; set; } = "アルファ";
+}
+
+class Beta : Alpha
+{
+    public new string Name { get; set; } = "ベータ";
+    public void Sub()
+    {
+        Console.WriteLine(base.Name);
+        Console.WriteLine(this.Name);
+    }
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var x = new Beta();
+        x.Sub();
+    }
+}
+
