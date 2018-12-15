8876dfd33f4a2999b9d3b11922eb63f7d966d523
Akira Kawamata <autumn@piedey.co.jp>
2018-12-15 19:05:35 +0900

    add case

---
+++ Answers/LanguageKeywords/case/case/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 0, 1, -1 };
+        foreach (var item in array)
+        {
+            switch (item)
+            {
+                case 0: Console.Write("ZERO "); break;
+                case 1: Console.Write("ONE "); break;
+                default: Console.Write("MASQUERADE"); break;
+            }
+        }
+    }
+}
