e1e8cd14909370ef5ded096fee81d2f287c8ea22
Akira Kawamata <autumn@piedey.co.jp>
2018-12-15 17:02:01 +0900

    add bool

---
+++ Answers/LanguageKeywords/bool/bool/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        bool x = a == b;
+        if (x == true) Console.WriteLine("aとbは同じです。");
+        if (x == false) Console.WriteLine("aとbは異なります。");
+    }
+}
