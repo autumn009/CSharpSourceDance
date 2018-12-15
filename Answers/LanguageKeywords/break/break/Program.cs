03dd8b4c31cf357a842a33c2593c97e16086b6fe
Akira Kawamata <autumn@piedey.co.jp>
2018-12-15 18:43:26 +0900

    add break

---
+++ Answers/LanguageKeywords/break/break/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (j == 1 && i == 1) break;
+                Console.Write($"{i}+{j}={i+j} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
