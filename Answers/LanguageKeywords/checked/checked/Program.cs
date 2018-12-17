34bba6747c99d3c9ba8e4c549306466faf28e0c6
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 15:40:41 +0900

    add checked

---
+++ Answers/LanguageKeywords/checked/checked/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    class Program
+{
+    static void Main()
+    {
+        int a = int.MaxValue;
+        int b = 1;
+        try
+        {
+            Console.WriteLine(a + b);
+            checked
+            {
+                Console.WriteLine(a + b);
+            }
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
