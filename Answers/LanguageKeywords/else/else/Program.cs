a7ed67185e8de04cee4c716f191ed6441909226f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 19:12:34 +0900

    add else

---
+++ Answers/LanguageKeywords/else/else/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 2;
+        if(x == 1)
+            Console.WriteLine("xは1です。");
+        else if( x == 2 )
+            Console.WriteLine("xは2です。");
+        else
+            Console.WriteLine("xは1でも2でありません。");
+    }
+}
