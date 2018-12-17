006fc50608b3b393699aeaf722207876c02d4b17
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 20:07:08 +0900

    add extern

---
+++ Answers/LanguageKeywords/extern/extern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+class ExternTest
+{
+    [DllImport("User32.dll", CharSet = CharSet.Unicode)]
+    public static extern int MessageBox(IntPtr h, string m, string c, int type);
+
+    static int Main()
+    {
+        return MessageBox((IntPtr)0, "Hello from Extern Sample", "Extern Sample", 0);
+    }
+}
