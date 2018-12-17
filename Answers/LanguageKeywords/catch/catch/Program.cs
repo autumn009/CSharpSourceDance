5c26244f5ab5e550cf796ff41322d5ceb8280c2e
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 14:56:17 +0900

    add catch

---
+++ Answers/LanguageKeywords/catch/catch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            int a = 1;
+            int b = 0;
+            Console.WriteLine(a/b);
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine($"変数bの値を確認して下さい。: {e}");
+        }
+    }
+}
