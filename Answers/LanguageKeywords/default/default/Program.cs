c1ca0135f69d46daa3322d5772034cd7bbdc42db
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 16:30:08 +0900

    add default

---
+++ Answers/LanguageKeywords/default/default/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = -999;
+        switch (n)
+        {
+            case 1: Console.WriteLine("ONE"); break;
+            case 2: Console.WriteLine("TWO"); break;
+            default: Console.WriteLine("ANOTHER"); break;
+        }
+    }
+}
