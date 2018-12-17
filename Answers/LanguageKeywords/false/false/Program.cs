6d2ec98cfe05e2f37925113918cd44f1b1c7be26
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 20:15:21 +0900

    add false

---
+++ Answers/LanguageKeywords/false/false/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 4649;
+        int b = 4649;
+        bool c = a == b;
+        if (c == true) Console.WriteLine("aはbと同じ");
+        if (c) Console.WriteLine("aはbと同じ");
+        if (c == false) Console.WriteLine("aはbと違う");
+    }
+}
