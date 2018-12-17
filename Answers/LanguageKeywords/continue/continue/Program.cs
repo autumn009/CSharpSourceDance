93bb95012ffdab13a1b4d9bba24de7e1173d3182
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 16:15:32 +0900

    add continue

---
+++ Answers/LanguageKeywords/continue/continue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        const int x = 100;
+        for (int i = -20; i <= 20; i += 10)
+        {
+            if (i == 0) continue;
+            Console.WriteLine(x / i);
+        }
+    }
+}
