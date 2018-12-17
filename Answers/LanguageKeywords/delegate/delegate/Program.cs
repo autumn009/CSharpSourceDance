21bdec8ee9a8338754e2214c8618f4854b879f8a
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 16:37:21 +0900

    add delegate

---
+++ Answers/LanguageKeywords/delegate/delegate/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+delegate void MyDelegate();
+
+class Program
+{
+    static void Main()
+    {
+        MyDelegate doit = delegate ()
+        {
+            Console.WriteLine("Hello World!");
+        };
+        doit();
+    }
+}
