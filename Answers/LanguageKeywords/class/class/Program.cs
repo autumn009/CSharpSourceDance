4c5145d374075008652506c4b3ca02492943c4e5
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 15:49:06 +0900

    add class

---
+++ Answers/LanguageKeywords/class/class/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class A
+{
+    private int a { get; set; } = 1;
+    public int b { get; set; } = 2;
+    public void sub()
+    {
+        Console.WriteLine(a + b);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new A();
+        a.b = 999;
+        a.sub();
+    }
+}
