e57604c1c6654b7de1abe65a3976c32dc3863fbe
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 19:50:15 +0900

    add event

---
+++ Answers/LanguageKeywords/event/event/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class X
+{
+    public event EventHandler OnErrorNotify;
+    public int Div (int x, int y)
+    {
+        if (y == 0)
+        {
+            OnErrorNotify(this, new EventArgs());
+            return 0;
+        }
+        else return x / y;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new X();
+        a.OnErrorNotify += (sender, e) =>
+        {
+            Console.WriteLine("オブジェクトaでエラー発生! 0による除算の可能性があります。");
+        };
+        Console.WriteLine(a.Div(1, 0));
+    }
+}
