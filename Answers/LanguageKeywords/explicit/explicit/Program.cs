1efc542ed36fb02cc2055b77280814b72d0e04e7
Akira Kawamata <autumn@piedey.co.jp>
2018-12-17 19:57:25 +0900

    add explicit

---
+++ Answers/LanguageKeywords/explicit/explicit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class A
+{
+    public int X;
+}
+
+class B
+{
+    public string X;
+    public static explicit operator B(A a)
+    {
+        return new B() { X = a.X.ToString() };
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A a = new A() { X = 4649 };
+        B b = (B)a;
+        Console.WriteLine(b.X);
+    }
+}
