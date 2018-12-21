4f08db6a5ae2e4ad8f4f16f6d2a30be8a08c9f12
Akira Kawamata <autumn@piedey.co.jp>
2018-12-21 17:22:23 +0900

    add foreach

---
+++ Answers/LanguageKeywords/foreach/foreach/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        foreach (var item in Enumerable.Range(0, 3).Reverse())
+        {
+            Console.Write(item);
+        }
+    }
+}
