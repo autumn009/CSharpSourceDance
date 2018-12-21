e26c95b494c35aea69c75ef8e3c2388d2d529f44
Akira Kawamata <autumn@piedey.co.jp>
2018-12-21 17:15:26 +0900

    add for

---
+++ Answers/LanguageKeywords/for/for/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool j = true;
+        for (int i = 0; j; i++)
+        {
+            if (i == 3) j = false;
+            Console.Write(i);
+        }
+    }
+}
