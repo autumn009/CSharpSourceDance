afe0c43288d5cc646f7d97746331eebb5cf0bdcb
Akira Kawamata <autumn@piedey.co.jp>
2018-12-21 16:47:28 +0900

    add fixed

---
+++ Answers/LanguageKeywords/fixed/fixed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Point
+{
+    public char[] a;
+}
+class Program
+{
+    unsafe static void Main()
+    {
+        // Variable pt is a managed variable, subject to garbage collection.
+        Point pt = new Point();
+        pt.a = "Hell!!".ToCharArray();
+        fixed (char* p = &pt.a[3])
+        {
+            *(p + 1) = 'o';
+        }
+        Console.WriteLine(new string(pt.a));
+    }
+}
