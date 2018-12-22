39e02b6cf76e3085000204dbdfe73a3bbd2a4709
Akira Kawamata <autumn@piedey.co.jp>
2018-12-22 16:43:26 +0900

    add if

---
+++ Answers/LanguageKeywords/if/if/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var x = 1;
+        var y = 2;
+        if (x == y)
+            Console.WriteLine("Best Match!");
+        else
+            Console.WriteLine("Not Match!");
+    }
+}
