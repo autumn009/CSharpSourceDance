5070a5a653d0598ef2dee79fd60b99b4fcece450
Akira Kawamata <autumn@piedey.co.jp>
2018-12-22 16:32:06 +0900

    add goto

---
+++ Answers/LanguageKeywords/goto/goto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (i == 1 && j == 1) goto exit;
+                Console.WriteLine($"{i}{j}");
+            }
+        }
+        exit:
+        Console.WriteLine("Done!");
+    }
+}
