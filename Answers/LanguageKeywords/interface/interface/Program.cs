07b2826c30e61d5f6c5f7624ee0514dfc507ad34
Akira Kawamata <autumn@piedey.co.jp>
2018-12-23 19:58:07 +0900

    add interface

---
+++ Answers/LanguageKeywords/interface/interface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+interface ISample
+{
+    void Sub();
+}
+
+class X : ISample
+{
+    public void Sub()
+    {
+        Console.WriteLine("I'm in X!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        ISample a = new X();
+        a.Sub();
+    }
+}
