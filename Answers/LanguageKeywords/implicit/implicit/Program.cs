19b47faf25513eb02312f15c124cb712a1ff7714
Akira Kawamata <autumn@piedey.co.jp>
2018-12-23 19:00:44 +0900

    add implicit

---
+++ Answers/LanguageKeywords/implicit/implicit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class A
+{
+    public int X;
+}
+
+class B
+{
+    public string X;
+    public static implicit operator B(A a)
+    {
+        return new B() { X = a.X.ToString() };
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A a = new A() { X = 4649 };
+        B b = a;
+        Console.WriteLine(b.X);
+    }
+}
