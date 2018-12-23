1a36d217cf712fd4c7ddb4d2322efd82ef13c613
Akira Kawamata <autumn@piedey.co.jp>
2018-12-23 19:46:07 +0900

    add in

---
+++ Answers/LanguageKeywords/in/in/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+interface IContravariant<in A>
+{
+    void X(A x);
+}
+
+class Sample<A> : IContravariant<A>
+{
+    public void X(A x)
+    {
+        Console.WriteLine(x);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        IContravariant<Object> iobj = new Sample<Object>();
+        IContravariant<String> istr;
+
+        istr = iobj;
+        istr.X("Hello Contravariant!");
+    }
+}
