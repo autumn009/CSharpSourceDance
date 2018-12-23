e3c5285361ece07b245478373b3e3d372c49578a
Akira Kawamata <autumn@piedey.co.jp>
2018-12-23 20:16:33 +0900

    add internal

---
+++ Answers/LanguageKeywords/internal/internal/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Sample
+{
+    internal int X = 4649;
+    private int Y = 4649;
+    protected int Z = 4649;
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        var a = new Sample();
+        Console.WriteLine(a.X);
+    }
+}
