40b9d14ba3901f343bf7efd3f263232cbc6dbf4f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:56:22 +0900

    add protected

---
+++ Answers/LanguageKeywords/protected/protected/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class A
+{
+    protected void subA()
+    {
+        Console.WriteLine("SubA");
+    }
+}
+
+class B:A
+{
+    internal void SubB()
+    {
+        subA();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new B();
+        x.SubB();
+    }
+}
