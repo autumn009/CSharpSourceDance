56f0bc10b6b691455f3c3dd25740b9d0b33a385f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:24:37 +0900

    add this

---
+++ Answers/LanguageKeywords/this/this/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class A
+{
+    private int p;
+    internal void Output() => Console.WriteLine(p);
+    internal A(int p)
+    {
+        this.p = p;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new A(4649);
+        x.Output();
+    }
+}
