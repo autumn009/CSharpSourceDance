740b90000d336faeeeadb31e447002239103fe84
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:05:22 +0900

    add null

---
+++ Answers/LanguageKeywords/null/null/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        var reader = new StringReader("A\r\nB\r\nC");
+        for (; ; )
+        {
+            var s = reader.ReadLine();
+            if (s == null) break;
+            Console.Write(s);
+        }
+    }
+}
