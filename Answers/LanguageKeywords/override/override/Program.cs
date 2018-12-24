82eb6983c0774cb36983138477a8071016770d67
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:41:35 +0900

    add override

---
+++ Answers/LanguageKeywords/override/override/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+abstract class A
+{
+    abstract internal void Sub();
+}
+
+class B : A
+{
+    internal override void Sub()
+    {
+        Console.WriteLine("I'm implemented!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A a = new B();
+        a.Sub();
+    }
+}
