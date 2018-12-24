f87b64e4aa83bad05632dec36b867b488944ad5f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:23:43 +0900

    add ref

---
+++ Answers/LanguageKeywords/ref/ref/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Sub(ref int a) => a++;
+
+    static void Main()
+    {
+        int x = 4648;
+        Sub(ref x);
+        Console.WriteLine(x);
+    }
+}
