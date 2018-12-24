b922660a661ba29fb38d73957763b8a3126fc64e
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:27:06 +0900

    add return

---
+++ Answers/LanguageKeywords/return/return/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var a = 1;
+        if (a != 0)
+        {
+            Console.WriteLine($"a is {a}");
+            return;
+        }
+        Console.WriteLine("a is zero");
+    }
+}
