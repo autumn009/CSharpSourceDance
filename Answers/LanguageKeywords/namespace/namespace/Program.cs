da39caaf8e022fc98d41a4ed69132dc5c90dceaf
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 14:50:21 +0900

    add namespace

---
+++ Answers/LanguageKeywords/namespace/namespace/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace A
+{
+    static class TheClass
+    {
+        internal static void Sub() => Console.WriteLine("In namespace A");
+    }
+}
+
+namespace B
+{
+    static class TheClass
+    {
+        internal static void Sub() => Console.WriteLine("In namespace B");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A.TheClass.Sub();
+        B.TheClass.Sub();
+    }
+}
