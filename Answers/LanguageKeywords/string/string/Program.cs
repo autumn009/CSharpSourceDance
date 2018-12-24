16fad43785c158c818ea96454f641d369f802f12
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:06:24 +0900

    add string

---
+++ Answers/LanguageKeywords/string/string/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s1 = "Hello";
+        string s2 = "World!";
+        string all = s1 + " " + s2;
+        Console.WriteLine(all);
+    }
+}
