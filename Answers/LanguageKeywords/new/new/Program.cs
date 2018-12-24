fa76719bc0d0c9580ae1edd1ccdfea76c3695092
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 14:57:14 +0900

    add new

---
+++ Answers/LanguageKeywords/new/new/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class A
+{
+    internal void Sub() => Console.WriteLine("In A");
+}
+
+class B:A
+{
+    internal new void Sub() => Console.WriteLine("In B");
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new B();
+        x.Sub();
+    }
+}
