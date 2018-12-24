c1e0387a850918eba667c7e5ec794eb06526eac6
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:01:22 +0900

    add static

---
+++ Answers/LanguageKeywords/static/static/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class A
+{
+    internal static int X { get; set; }
+    internal void Sub(int x) => X = x;
+    internal void Output() => Console.WriteLine(X);
+}
+
+class Program
+{
+    static void Main()
+    {
+        var p = new A();
+        var q = new A();
+        p.Sub(1);
+        q.Sub(2);
+        p.Output();
+    }
+}
