0d4b289fe80843722824abf9c8b8f50579bc9c67
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 14:23:58 +0900

    add lock

---
+++ Answers/LanguageKeywords/lock/lock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        object sync = new object();
+        Action act = () =>
+        {
+            for (; ; )
+            {
+                lock (sync)
+                {
+                    Console.Write("O");
+                    Console.Write("K");
+                }
+            }
+        };
+        Parallel.Invoke(act, act);
+    }
+}
