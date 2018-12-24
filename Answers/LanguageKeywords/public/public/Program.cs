d5aa5e98b85b474a4834cea8aec430427fc4b137
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:03:15 +0900

    add public

---
+++ Answers/LanguageKeywords/public/public/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class A
+{
+    public void Sub() => Console.WriteLine("Everybody Welcome!");
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new A();
+        a.Sub();
+    }
+}
