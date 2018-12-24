bf0cc1dea3a1d019c61a8a1a91150a59718fa3c3
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:38:08 +0900

    add sealed

---
+++ Answers/LanguageKeywords/sealed/sealed/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+sealed class A
+{
+    internal void Sub()
+    {
+        Console.WriteLine("Don't inherit A!");
+    }
+}
+
+//class B : A { }
+
+class Program
+{
+    static void Main()
+    {
+        var x = new A();
+        x.Sub();
+    }
+}
