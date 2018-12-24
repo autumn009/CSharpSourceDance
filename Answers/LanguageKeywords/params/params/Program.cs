4408359627379511c1faf1e8c31216e09117aa20
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:44:09 +0900

    add params

---
+++ Answers/LanguageKeywords/params/params/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Sub(params int[] array)
+    {
+        Console.WriteLine($"sum is {array.Sum()}");
+    }
+
+    static void Main(string[] args)
+    {
+        Sub(1, 2, 3);
+        Sub(4, 5);
+    }
+}
