1fc09ef2a66d480f97b71b779389e6604b9847ee
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:30:16 +0900

    add throw

---
+++ Answers/LanguageKeywords/throw/throw/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            throw new DivideByZeroException();
+        }
+        catch (DivideByZeroException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
