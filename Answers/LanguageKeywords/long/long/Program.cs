f4164f0e0e75aa710a83913d042043eadb1c3b42
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 14:35:46 +0900

    add long

---
+++ Answers/LanguageKeywords/long/long/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var max = int.MaxValue;
+        Console.WriteLine(max);
+        var maxPlus1 = max + 1L;
+        Console.WriteLine(maxPlus1);
+    }
+}
