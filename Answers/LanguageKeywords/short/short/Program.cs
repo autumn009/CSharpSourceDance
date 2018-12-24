265861e037e4708782ff284a96799b9e2388db5f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:47:03 +0900

    add short

---
+++ Answers/LanguageKeywords/short/short/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        unchecked
+        {
+            short a = short.MaxValue;
+            Console.WriteLine(a);
+            a++;
+            Console.WriteLine(a);
+        }
+    }
+}
