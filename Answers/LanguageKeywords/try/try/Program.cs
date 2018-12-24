bb99d229112091623c7e465228f6be0a4ff70b2f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:37:30 +0900

    add try

---
+++ Answers/LanguageKeywords/try/try/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch(Exception)
+        {
+            Console.WriteLine("catch");
+        }
+        finally
+        {
+            Console.WriteLine("finally");
+        }
+    }
+}
+
