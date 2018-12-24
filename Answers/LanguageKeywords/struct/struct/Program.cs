e2636cd5a6beee490778f0ddb974d17081c8c13a
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:12:15 +0900

    add struct

---
+++ Answers/LanguageKeywords/struct/struct/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+struct A
+{
+    internal int X { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        A a = new A();
+        a.X = 4648;
+        A b = a;
+        b.X++;
+        Console.WriteLine(a.X);
+        Console.WriteLine(b.X);
+    }
+}
