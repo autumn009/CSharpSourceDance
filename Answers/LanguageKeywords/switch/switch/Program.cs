96fe979d2baff1aef79140dae2fe2511a63fde18
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 19:17:36 +0900

    add switch

---
+++ Answers/LanguageKeywords/switch/switch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            switch (i)
+            {
+                case 0: Console.Write("ZERO "); break;
+                case 1: Console.Write("ONE "); break;
+                case 2: Console.Write("TWO "); break;
+                default: Console.WriteLine("? "); break;
+            }
+        }
+    }
+}
