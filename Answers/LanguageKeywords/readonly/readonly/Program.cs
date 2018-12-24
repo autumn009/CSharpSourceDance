1a7711de2573e20edbecde99eb20de13c99cf89f
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:19:30 +0900

    add readonly

---
+++ Answers/LanguageKeywords/readonly/readonly/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class X
+{
+    public readonly int A;
+    public X(int a) => A = a;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new X(4649);
+        Console.WriteLine(x.A);
+    }
+}
