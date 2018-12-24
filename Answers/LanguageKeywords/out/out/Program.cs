0b8b26cdeab607a60880ef7072625015fbb676bd
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:29:05 +0900

    add out

---
+++ Answers/LanguageKeywords/out/out/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    private static void sub(out int x, out int y)
+    {
+        x = 4649;
+        y = 4989;
+    }
+
+    static void Main()
+    {
+        int x, y;
+        sub(out x, out y);
+        Console.WriteLine($"x={x} y={y}");
+    }
+}
