73222083ec49167c57c3b56f35315579cdb624bc
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:12:30 +0900

    add object

---
+++ Answers/LanguageKeywords/object/object/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+    class Program
+{
+    static void Main()
+    {
+        object[] array = { "Hello!", 4639, new Program() };
+        foreach (var item in array) Console.WriteLine(item.GetType().FullName);
+    }
+}
