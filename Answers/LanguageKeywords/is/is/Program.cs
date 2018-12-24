bca9fc58a05ef99cedb2477f22725a3fdb139034
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 11:48:28 +0900

    add is

---
+++ Answers/LanguageKeywords/is/is/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        object[] array = { 1, 2, "Three", 4 };
+        int sum = 0;
+        foreach (var item in array)
+        {
+            if (item is int) sum += (int)item;
+        }
+        Console.WriteLine(sum);
+    }
+}
+
+
