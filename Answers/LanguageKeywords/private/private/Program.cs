fa4dfd1e3d913919ec28e94b78c055d3edc0f9a3
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 15:51:00 +0900

    add private

---
+++ Answers/LanguageKeywords/private/private/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class X
+{
+    private int a;
+    public int A { get => a; set => a = value; }
+    public void Sub() => Console.WriteLine(a);
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new X();
+        x.A = 4649;
+        x.Sub();
+    }
+}
