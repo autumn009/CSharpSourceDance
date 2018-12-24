47f059bd2947989b594c53bca5b96cf446d22417
Akira Kawamata <autumn@piedey.co.jp>
2018-12-24 16:51:25 +0900

    add sizeof

---
+++ Answers/LanguageKeywords/sizeof/sizeof/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(sizeof(byte));
+        Console.WriteLine(sizeof(short));
+        Console.WriteLine(sizeof(int));
+        Console.WriteLine(sizeof(long));
+    }
+}
