24ad578219472dbc1726425e1827a249fb286fbb
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 11:20:14 +0900

    add while

---
+++ Answers/LanguageKeywords/while/while/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 0;
+        while (a < 10)
+        {
+            Console.Write(a);
+            a++;
+        }
+    }
+}
