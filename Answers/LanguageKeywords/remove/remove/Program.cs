964a59d28f1ed02ee4fa2c77e41eb0e2c2dfcc6d
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 14:26:45 +0900

    add remove

---
+++ Answers/LanguageKeywords/remove/remove/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+class X
+{
+    public List<EventHandler> handlers = new List<EventHandler>();
+    public event EventHandler OnNotify
+    {
+        add
+        {
+            handlers.Add(value);
+        }
+        remove
+        {
+            handlers.Remove(value);
+        }
+    }
+    public void KickHandler()
+    {
+        var e = new EventArgs();
+        foreach (var item in handlers) item(this, e);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new X();
+        EventHandler handler = (sender, e) =>
+        {
+            Console.WriteLine("handler called!");
+        };
+        a.OnNotify += handler;
+        a.KickHandler();
+        a.OnNotify -= handler;
+        a.KickHandler();
+    }
+}
