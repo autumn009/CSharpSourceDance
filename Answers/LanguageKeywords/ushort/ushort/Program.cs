d2e0d40ca738c0235383a2a1bead1bfad730e9d9
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 10:29:27 +0900

    add ushort

---
+++ Answers/LanguageKeywords/ushort/ushort/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        ushort a = 65534;
+        ushort b = 1;
+        ushort x = (ushort)(a + b);
+        Console.WriteLine(x);
+    }
+}
