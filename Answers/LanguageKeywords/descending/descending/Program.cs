b72ecb2e229dda9b8e4358f13a13bbfac8dec9f1
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 15:01:17 +0900

    add ascending and descending

---
+++ Answers/LanguageKeywords/descending/descending/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 3, 1, 2 };
+        foreach (var item in from n in array orderby n descending select n) Console.Write(item);
+    }
+}
