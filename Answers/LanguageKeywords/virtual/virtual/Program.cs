2494951bbf6a8401dfe36b1921d558023f36f9ac
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 10:48:30 +0900

    add virtual

---
+++ Answers/LanguageKeywords/virtual/virtual/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class A
+{
+    internal virtual void Sub()
+    {
+        Console.WriteLine("A.Sub();");
+    }
+}
+
+class B:A
+{
+    internal override void Sub()
+    {
+        Console.WriteLine("B.Sub();");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        A a = new B();
+        a.Sub();
+    }
+}
