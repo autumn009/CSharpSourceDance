404555380830485d2b3d419db661a5925d2d73db
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 10:44:14 +0900

    add using

---
+++ Answers/LanguageKeywords/using/using/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class X : IDisposable
+{
+    public void Dispose()
+    {
+        Console.WriteLine("X.Dispose!"); ;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        using (var x = new X())
+        {
+            return;
+        }
+        Console.WriteLine("Never Run");
+    }
+}
