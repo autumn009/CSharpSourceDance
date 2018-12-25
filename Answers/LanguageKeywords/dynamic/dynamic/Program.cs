ce8f6ad84f231e613e84eb6361e440cff83f9f97
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 15:33:59 +0900

    add dynamic

---
+++ Answers/LanguageKeywords/dynamic/dynamic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class X
+{
+    internal void Sub1() => Console.WriteLine("internal Sub");
+    private void Sub2() => Console.WriteLine("private Sub");
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            dynamic x = new X();
+            x.Sub1();
+            x.Sub2();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
