3b2914f3825bc4082ed611e663e893afca7aee1e
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 10:58:32 +0900

    add void

---
+++ Answers/LanguageKeywords/void/void/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Sub1()
+    {
+        Console.WriteLine("In Sub1");
+    }
+
+    static int Sub2()
+    {
+        Console.WriteLine("In Sub2");
+        return 4849;
+    }
+
+    static void Main()
+    {
+        Sub1();
+        Sub2();
+    }
+}
