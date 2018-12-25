553a2883f20304bc65429dc1ae915b7682e3db5e
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 16:07:56 +0900

    add group

---
+++ Answers/LanguageKeywords/group/group/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Alice", "Betty", "Anton", "Bob" };
+        var q = from n in names group n by n.Substring(0, 1);
+        foreach (var item in q)
+            foreach (var m in item)
+                Console.WriteLine(m);
+    }
+}
