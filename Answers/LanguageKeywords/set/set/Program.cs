d774299a17d6d6ed08d85d27ffa15a6dc17afaf7
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 15:51:20 +0900

    add set

---
+++ Answers/LanguageKeywords/set/set/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    private static int prop;
+    static int Prop
+    {
+        get { return prop; }
+        set { prop = value; }
+    }
+
+    static void Main()
+    {
+        Prop = 4649;
+        Console.WriteLine(Prop);
+    }
+}
