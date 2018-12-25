f177b790aa823ee40eda230a3087284d6cc19cd0
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 15:45:09 +0900

    add from and get

---
+++ Answers/LanguageKeywords/from/from/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var q = from n in Enumerable.Range(1, 3) select n;
+        foreach (var item in q) Console.Write(item);
+    }
+}
---
+++ Answers/LanguageKeywords/get/get/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static int Prop { get; set; }
+
+    static void Main()
+    {
+        Prop = 4649;
+        Console.WriteLine(Program.Prop);
+    }
+}
