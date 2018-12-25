13afc95dfd7baa9b4e2597988d49379cf62590c1
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 16:58:14 +0900

    add let

---
+++ Answers/LanguageKeywords/let/let/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Alice", "Bob" };
+        foreach (var item in from n in names let m = n.ToUpper() where m.Count() >= 5 select m)
+            Console.WriteLine(item);
+    }
+}
