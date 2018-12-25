5f3b8f5796dddb5afe9e03e45736fa0734a7df00
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 17:03:59 +0900

    add groupby

---
+++ Answers/LanguageKeywords/orderby/orderby/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] array = { "Babe", "Adam", "Cyclon" };
+        foreach (var item in from n in array orderby n select n) Console.WriteLine(item);
+    }
+}
