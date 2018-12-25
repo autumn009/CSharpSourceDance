459029e15283e0fec26b8112e5460a5eab9db983
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 15:25:28 +0900

    add await

---
+++ Answers/LanguageKeywords/await/await/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        Task t = Task.Delay(1000);
+        Console.WriteLine("Wait 1 sec");
+        await t;
+        Console.WriteLine("Finish!");
+    }
+}
