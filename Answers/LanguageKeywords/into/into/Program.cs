15ef90b5c4be683b485b9a2613b5a5c452cbddf3
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 16:29:32 +0900

    add into

---
+++ Answers/LanguageKeywords/into/into/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Alice", "Betty", "Anton", "Bob", "Cindy" };
+        var q = from n in names group n by n.Substring(0, 1) into g where g.Count() >= 2 select g;
+        foreach (var item in q)
+            foreach (var m in item)
+                Console.WriteLine(m);
+    }
+}
