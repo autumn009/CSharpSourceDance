5ae2e242f2bdb6546b626b364c0d74d775744c2d
Akira Kawamata <autumn@piedey.co.jp>
2018-12-25 11:11:03 +0900

    add volatile

---
+++ Answers/LanguageKeywords/volatile/volatile/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static volatile int x;
+
+    static async Task Main()
+    {
+       _ = Task.Run(()=> {
+            for (int i = 0; ; i++)
+            {
+                x++;
+            }
+        });
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(x);
+            await Task.Delay(500);
+        }
+    }
+}
