e85cacb770718871c19eeb44a2007c3c2174df79
Akira Kawamata <autumn@piedey.co.jp>
2018-12-26 16:32:09 +0900

    add select

---
+++ Answers/LanguageKeywords/select/select/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        var q = from n in array where n >= 3 && n <= 6 select n * 10;
+        foreach (var item in q) Console.WriteLine(item);
+    }
+}
