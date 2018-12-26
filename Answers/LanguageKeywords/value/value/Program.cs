17e6caef1925a1978f28efb2b1d2488e95b38494
Akira Kawamata <autumn@piedey.co.jp>
2018-12-26 16:42:32 +0900

    add value

---
+++ Answers/LanguageKeywords/value/value/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static int Prop
+    {
+        set
+        {
+            Console.WriteLine($"プロパティPropに{value}を設定しようとしています。");
+        }
+    }
+
+    static void Main()
+    {
+        Prop = 4649;
+    }
+}
