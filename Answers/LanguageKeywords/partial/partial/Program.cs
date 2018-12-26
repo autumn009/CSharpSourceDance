1a24fe6ec6162db63c475a09b41602fafdd691bc
Akira Kawamata <autumn@piedey.co.jp>
2018-12-26 16:15:55 +0900

    add partial

---
+++ Answers/LanguageKeywords/partial/partial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+partial class A
+{
+    internal void Sub1() => Console.WriteLine("I'm Sub1");
+}
+
+partial class A
+{
+    internal void Sub2() => Console.WriteLine("I'm Sub2");
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new A();
+        a.Sub1();
+        a.Sub2();
+    }
+}
