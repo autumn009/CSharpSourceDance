aadea57e8fd8baef9f8d1b5c1085f1009f459f0d
Akira Kawamata <autumn@piedey.co.jp>
2018-12-26 18:51:03 +0900

    add where

---
+++ Answers/LanguageKeywords/where/where/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class A
+{
+    internal void Sub() => Console.WriteLine("I'm A.Sub");
+}
+
+
+class B<T> where T : A
+{
+    internal void Sub(T t) => t.Sub();
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        var x = new B<A>();
+        x.Sub(new A());
+    }
+}
