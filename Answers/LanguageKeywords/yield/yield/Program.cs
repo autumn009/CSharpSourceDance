33c6164180797152196ba8c1608c969a4cc969a4
Akira Kawamata <autumn@piedey.co.jp>
2018-12-26 18:57:23 +0900

    add yield

---
+++ Answers/LanguageKeywords/yield/yield/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    private static IEnumerable<int> getIt()
+    {
+        int n = 1;
+        for (; ; )
+        {
+            yield return n;
+            n *= 2;
+            if (n > 100) yield break;
+        }
+    }
+
+
+    static void Main()
+    {
+        foreach (var item in getIt()) Console.Write($"{item} ");
+    }
+}
