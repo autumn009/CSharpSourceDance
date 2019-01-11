f3f06b9d852adb926912fdece1a4ef98aa6755aa
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 16:40:52 +0900

    add pointerAccess

---
+++ Answers/SymbolOperators/pointerAccess/pointerAccess/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+struct MyStruct
+{
+    public int v;
+}
+
+class Program
+{
+    static unsafe void Main()
+    {
+        MyStruct theValue;
+        MyStruct* p = &theValue;
+        p->v = 4649;
+        Console.WriteLine(theValue.v);
+    }
+}
