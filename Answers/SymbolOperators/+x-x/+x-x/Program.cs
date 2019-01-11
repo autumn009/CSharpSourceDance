e96e4df40475d0faebb0a5478656a8ab3170c305
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 20:19:20 +0900

    add +x/-x

---
+++ Answers/SymbolOperators/+x-x/+x-x/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 4649;
+        int b = -a;
+        int c = -b;
+        int d = +c;
+        Console.WriteLine(d);
+    }
+}
