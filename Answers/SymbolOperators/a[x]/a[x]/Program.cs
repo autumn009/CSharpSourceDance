ae592a3d9e34bc61e129940fea735bfff0824660
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 16:19:11 +0900

    add a[x]

---
+++ Answers/SymbolOperators/a[x]/a[x]/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var list = new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        list[7] = 4649;
+        list.Insert(0, -1);
+        Console.WriteLine(list[8]);
+    }
+}
