a66e7084938fbe99d07322a1c132bff59f34b64f
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 11:50:05 +0900

    add dot

---
+++ Answers/SymbolOperators/dot/dot/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MyNS
+{
+    class MyClass
+    {
+        internal static void MyMethod1()
+        {
+            Console.WriteLine("MyMethod1 Called");
+        }
+        internal void MyMethod2()
+        {
+            Console.WriteLine("MyMethod2 Called");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        MyNS.MyClass.MyMethod1();
+        var x = new MyNS.MyClass();
+        x.MyMethod2();
+    }
+}
+
