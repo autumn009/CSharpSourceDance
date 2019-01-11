1f9dccd919eb8cab15dd5190221ab0c61e79308b
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 11:55:28 +0900

    add f(x)

---
+++ Answers/SymbolOperators/f(x)/f(x)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MyClass
+{
+    internal void MyMethod() => Console.WriteLine("MyMethod called!");
+}
+
+class Program
+{
+    static void Main()
+    {
+        var x = new MyClass();
+        x.MyMethod();
+    }
+}
