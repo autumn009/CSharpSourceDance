93645ae5462d9091e2a0d28cb4719212b3db072e
Akira Kawamata <autumn@piedey.co.jp>
2019-01-11 16:29:47 +0900

    add x++x--++x--x

---
+++ Answers/SymbolOperators/x++x--++x--x/x++x--++x--x/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] a = { 0, 1, 2, 3 };
+        int p = 1;
+        Console.WriteLine(a[p++]);
+        Console.WriteLine(a[p]);
+        Console.WriteLine(a[--p]);
+    }
+}
