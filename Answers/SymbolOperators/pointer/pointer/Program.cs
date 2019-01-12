1033b340fbf7cf4393bceecf2bee87a6cc6c5413
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 16:23:56 +0900

    add *x

---
+++ Answers/SymbolOperators/pointer/pointer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static unsafe void Main()
+    {
+        int theValue = 4649 / 2;
+        int* p = &theValue;
+        int r = *p + *p + 1;
+        Console.WriteLine(r);
+    }
+}
