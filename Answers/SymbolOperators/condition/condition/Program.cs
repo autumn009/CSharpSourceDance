97e30904a425cd0304d80c3c691afeb79f86fd2d
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 19:31:09 +0900

    add conditon

---
+++ Answers/SymbolOperators/condition/condition/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = 2;
+        if (a == b) Console.WriteLine("aとbは同じです。");
+        if (a != b) Console.WriteLine("aとbは違います。");
+        if (a > b) Console.WriteLine("aはbより大です。");
+        if (a < b) Console.WriteLine("aはbより小です。");
+        if (a >= b) Console.WriteLine("aはb以上です。");
+        if (a <= b) Console.WriteLine("aはb以下です。");
+    }
+}
