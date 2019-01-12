0ffe1f2fc8a61d6b48fc6f8c4de6fcff3e058c00
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 16:05:27 +0900

    add (T)x

---
+++ Answers/SymbolOperators/(T)x/(T)x/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 4649;
+        short b = (short)a;
+        Console.WriteLine(b);
+        sbyte c = (sbyte)b;
+        Console.WriteLine(c);
+    }
+}
