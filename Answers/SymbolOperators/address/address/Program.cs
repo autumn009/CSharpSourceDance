b6854f468accfe7d1f1082150c31552bc5e971e8
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 16:16:37 +0900

    add &x

---
+++ Answers/SymbolOperators/address/address/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+struct MyStruct
+{
+    public void Sub() => Console.WriteLine("Hello in MyStruct");
+}
+
+class Program
+{
+    static unsafe void Main()
+    {
+        MyStruct theValue;
+        MyStruct* p = &theValue;
+        p->Sub();
+    }
+}
