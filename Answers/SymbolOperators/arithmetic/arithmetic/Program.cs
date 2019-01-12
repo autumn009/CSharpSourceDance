64ff9a07e92f36ace7b9989fc5786a9391c7754d
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 16:33:10 +0900

    add arithmetic

---
+++ Answers/SymbolOperators/arithmetic/arithmetic/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 3;
+        int b = 2;
+        Console.WriteLine(a + b);
+        Console.WriteLine(a - b);
+        Console.WriteLine(a * b);
+        Console.WriteLine(a / b);
+        Console.WriteLine(a % b);
+    }
+}
