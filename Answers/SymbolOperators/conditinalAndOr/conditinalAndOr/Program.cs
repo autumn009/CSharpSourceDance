509aba526ad4d9047829cb545673768eeebbff9d
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 19:57:51 +0900

    add conditinalAndOr

---
+++ Answers/SymbolOperators/conditinalAndOr/conditinalAndOr/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    private static bool sub2()
+    {
+        Console.WriteLine("In sub2");
+        return false;
+    }
+
+    private static bool sub1()
+    {
+        Console.WriteLine("In sub1");
+        return true;
+    }
+    static void Main()
+    {
+        if (sub1() || sub2()) Console.WriteLine("Nice Day!");
+        if (sub2() && sub1()) Console.WriteLine("Good Day!");
+    }
+}
