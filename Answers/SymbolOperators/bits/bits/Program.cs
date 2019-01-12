d338a6b5a3039b98802fc32eb64c274bd750f652
Akira Kawamata <autumn@piedey.co.jp>
2019-01-12 19:36:34 +0900

    add bits

---
+++ Answers/SymbolOperators/bits/bits/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = 1;
+        int b = a | 2;
+        int c = b & 2;
+        int d = c ^ 1;
+        Console.WriteLine(d);
+    }
+}
