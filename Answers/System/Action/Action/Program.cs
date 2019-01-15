78a900944dfda583364eebf23edcee2a19057db3
Akira Kawamata <autumn@piedey.co.jp>
2019-01-15 19:45:21 +0900

    add Action

---
+++ Answers/System/Action/Action/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static Action sub(Action action)
+    {
+        return () =>
+        {
+            Console.Write("Hello ");
+            action();
+        };
+    }
+
+    static void Main()
+    {
+        var fullaction = sub(() =>
+        {
+            Console.Write("World!");
+        });
+        Console.Write("Let's say '");
+        fullaction();
+        Console.WriteLine("'");
+    }
+}
