944370fb2dd40acc335bdbb980b5a9fb63279661
Akira Kawamata <autumn@piedey.co.jp>
2019-01-15 19:06:34 +0900

    add x=y

---
+++ Answers/SymbolOperators/x=y/x=y/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class X
+{
+    internal int Y;
+}
+
+class Program
+{
+    static void Main()
+    {
+        var a = new X();
+        a.Y = 123;
+        var b = new X();
+        b.Y = 456;
+        a = b;
+        Console.WriteLine(a.Y);
+    }
+}
