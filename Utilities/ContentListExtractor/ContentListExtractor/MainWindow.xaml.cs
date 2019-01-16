3e4e6d9453f5d9cc78b257843c64b035e3897116
Akira Kawamata <autumn@piedey.co.jp>
2019-01-16 14:43:52 +0900

    add srcpath handler

--- Utilities/ContentListExtractor/ContentListExtractor/MainWindow.xaml.cs
+++ Utilities/ContentListExtractor/ContentListExtractor/MainWindow.xaml.cs
@@ -23,6 +23,43 @@ namespace ContentListExtractor
         public MainWindow()
         {
             InitializeComponent();
+            SrcPath.TextChanged += (object sender, TextChangedEventArgs e) =>
+            {
+                StartButton.IsEnabled = System.IO.File.Exists(SrcPath.Text);
+            };
+        }
+
+        private void SrcButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new System.Windows.Forms.OpenFileDialog();
+            fileDialog.InitialDirectory = System.IO.Path.GetDirectoryName(SrcPath.Text);
+            var result = fileDialog.ShowDialog();
+            switch (result)
+            {
+                case System.Windows.Forms.DialogResult.OK:
+                    var file = fileDialog.FileName;
+                    SrcPath.Text = file;
+                    SrcPath.ToolTip = file;
+                    break;
+                case System.Windows.Forms.DialogResult.Cancel:
+                default:
+                    break;
+            }
+        }
+
+        private void DstButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
